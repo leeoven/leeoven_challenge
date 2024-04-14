f3cefee3524bd6ac7924f93efba54fdd908cb6cb
Lee B <sigfuodhran0@gmail.com>
2024-04-14 16:56:39 +0000

    adds terraform main codes

--- backend-state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket                = "terraform-state-bucket-leeoven-com"
-    key                   = "website/terraform.tfstate"
-    region                = "us-east-1"
-    dynamodb_table        = "terraform_state_locks"
-  }
-}
